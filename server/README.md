5e986a0746f7e495c2d26c19adc5b369fe69c4a0
xiaoqiang-zhao <longze_xq@163.com>
2020-03-26 20:21:29 +0800

    first commit

---
+++ server/README.md
@@ -0,0 +1,10 @@
+# 简易服务器
+
+## 启动命令
+
+    node server
+    // 网站默认端口9999，可在 server/lib/config.js 中配置
+    
+## 配置文件说明
+    
+一些常量在 `server/lib/config.js` 中配置。
---
+++ src/pages/home/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    译文号杨帆启航 H5 宣传页
+  </div>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      text: '',
+      picUrl: ''
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+</style>
